01aca44a06639d9bebec21d4aeff17df3299099a
Swapnil Nanavati <201801083@daiict.ac.in>
2021-12-08 20:25:29 +0530

    css change
    styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,49 +1,57 @@
-import React,{useState} from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "../styles/Navigation.css";
+import { Container } from "react-bootstrap";
 
-import { Container } from 'react-bootstrap';
+import { Form, FormControl, Button } from "react-bootstrap";
+const Navigation = ({ ApiGet, Rename }) => {
+  const handleClick = () => {
+    let x = "";
+    x = document.getElementById("name").value;
+    //console.log(x);
+    Rename(x);
+  };
 
-import {Form, FormControl, Button, } from 'react-bootstrap'
- const Navigation = ({ApiGet, Rename}) => {
-    
-    const handleClick = () =>{
-        let x ='';
-        x=document.getElementById('name').value;
-        //console.log(x);
-        Rename(x)
-    }
-    
-    return (
-        <div>
+  return (
+    <div>
+      <Navbar
+        style={{ width: "100%", height: "60px" }}
+        bg="dark"
+        expand="lg"
+        header
+      >
+        <Container>
+          <div className="navigation__brand" href="#home">
+            Crypto Prediction
+          </div>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          {/* <Nav.Link href="#features">Overview</Nav.Link>
+          <Nav.Link href="#pricing">Blog</Nav.Link> */}
+          <div className="navbar-collapse justify-content-center ">
+            <Form className="d-flex ">
+              <FormControl
+                id="name"
+                type="search"
+                name="myCountry"
+                placeholder="Search Stocks..."
+                className="me-2 autocomplete"
+                aria-label="Search"
+                onKeyUp={handleClick}
+                style={{ width: "500px" }}
+              />
 
-            <Navbar bg="light" expand="lg" header>
-            <Container fluid>
-            <Navbar.Brand href="#home">Crypto Prediction</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Nav.Link href="#features">Overview</Nav.Link>
-                <Nav.Link href="#pricing">Blog</Nav.Link>
-                <div className="navbar-collapse justify-content-end ">
-                <Form className="d-flex ">
-                    <FormControl
-                        id="name"
-                        type="search"
-                        name="myCountry"
-                        placeholder="Search Stocks..."
-                        className="me-2 autocomplete"
-                        aria-label="Search"
-                        onKeyUp= {handleClick}
-                    />
-                    
-                    <Button variant="outline-success" className="autocomplete" onClick={ApiGet}>Search</Button>
-                    
-                    </Form>
-
-                
-                    
-                </div>
-            </Container>
-            </Navbar>
-        </div>
-    );
-}
-export default Navigation
+              <Button
+                variant="outline-success"
+                className="autocomplete"
+                onClick={ApiGet}
+              >
+                Search
+              </Button>
+            </Form>
+          </div>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+export default Navigation;
--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -1,65 +1,82 @@
-import React from 'react'
-import { Container, Card, Row, Col, Button } from 'react-bootstrap'
+import React from "react";
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import "../styles/RightPanel.css";
 
-const RightPanel = ({todayHigh, todayLow, volume, marketCap}) => {
-    //let StockSymbol={Name};
-    //console.log(Name);
-    // useEffect(() => {
-    //     effect
-    //     return () => {
-    //         cleanup
-    //     }
-    // }, [input])
-   // console.log(todayLow);
-    return (
-        <div>
-            <Container >
-                <Row className="align-items-center">
+const RightPanel = ({ todayHigh, todayLow, volume, marketCap }) => {
+  //let StockSymbol={Name};
+  //console.log(Name);
+  // useEffect(() => {
+  //     effect
+  //     return () => {
+  //         cleanup
+  //     }
+  // }, [input])
+  // console.log(todayLow);
+  if (!todayHigh) return <div className="rightpanel__main" />;
+  return (
+    <div className="rightpanel__main">
+      <div className="rightpanel__keymatrics">Key Matrics</div>
+      <Row>
+        <Col>
+          <div className="rightpanel__first">Today's High</div>
+          <div className="rightpanel__second">${todayHigh}</div>
+        </Col>
+        <Col>
+          <div className="rightpanel__first">Today's Low</div>
+          <div className="rightpanel__second">${todayLow}</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="rightpanel__first">24h Volume</div>
+          <div className="rightpanel__second">${volume}</div>
+        </Col>
+        <Col>
+          <div className="rightpanel__first">Market Cap</div>
+          <div className="rightpanel__second">${marketCap}</div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-                    <Col>
-                    <Card style={{ width: '18rem' ,height: '20rem' }}>
-                        
-                        <Card.Body>
-                            <Row>
-                                <Col>
-                                <Card.Title>Today's High</Card.Title>
-                                <Card.Text >${todayHigh}</Card.Text>
-                                </Col>
-                                <Col>
-                                <Card.Title>Today's low</Card.Title>
-                                <Card.Text >${todayLow}</Card.Text>
-                                </Col>
-                                
-                            </Row>
-                            <Row>
-                                <Col>
-                                <Card.Title>24h Volume</Card.Title>
-                                <Card.Text >${volume}</Card.Text>
-                                </Col>
-                                <Col>
-                                <Card.Title>Market Cap</Card.Title>
-                                <Card.Text >${marketCap}</Card.Text>
-                                </Col>
-                            
-                            
-                            </Row>
-                           
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                    <Col>
-                    <Card style={{ width: '18rem' ,height: '20rem' }}>
-                    <Card.Body>
-                            <Card.Title >Prediction</Card.Title>
-                            <Button variant="primary">Predict</Button>
-                            </Card.Body>
-                    </Card>
-                    </Col>
-                </Row>
-                
-            </Container>
-        </div>
-    )
-}
+export default RightPanel;
 
-export default RightPanel
+//  <Container>
+//    <Row className="align-items-center">
+//      <Col className="rightpanel__main">
+//        <Card style={{ height: "20rem" }}>
+//          <Card.Body>
+//            <Row>
+//              <Col>
+//                <Card.Title>Today's High</Card.Title>
+//                <Card.Text>${todayHigh}</Card.Text>
+//              </Col>
+//              <Col>
+//                <Card.Title>Today's low</Card.Title>
+//                <Card.Text>${todayLow}</Card.Text>
+//              </Col>
+//            </Row>
+//            <Row>
+//              <Col>
+//                <Card.Title>24h Volume</Card.Title>
+//                <Card.Text>${volume}</Card.Text>
+//              </Col>
+//              <Col>
+//                <Card.Title>Market Cap</Card.Title>
+//                <Card.Text>${marketCap}</Card.Text>
+//              </Col>
+//            </Row>
+//          </Card.Body>
+//        </Card>
+//      </Col>
+//      <Col>
+//  <Card style={{ width: "18rem", height: "20rem" }}>
+//    <Card.Body>
+//      <Card.Title>Prediction</Card.Title>
+//      <Button variant="primary">Predict</Button>
+//    </Card.Body>
+//  </Card>
+//      </Col>
+//    </Row>
+//  </Container>;
